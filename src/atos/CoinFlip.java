b860c54f23b865b52df5e3a79cf36a258b157238
MarinaNguessan <marinanguessan23@gmail.com>
2023-02-02 02:48:34 +0000

    first commit

---
+++ src/atos/CoinFlip.java
@@ -0,0 +1,29 @@
+package atos;
+import java.util.Random;
+
+public class CoinFlip {
+
+	public static void main(String[] args) {
+ // Création d'un objet pour générer des nombres aléatoires
+        
+        Random rand = new Random();
+        
+        // Choisir un nombre aléatoire entre 0 et 1
+        
+        int n = rand.nextInt(2);
+        
+        // Afficher le nombre choisi
+        
+        System.out.println("Valeur aléatoire choisie: " + n);
+
+        // Si n est égal à 0, afficher "Pile"
+        if (n == 0) {
+            System.out.println("Pile");
+        }
+            // Sinon, afficher "Tail"
+        else {
+            System.out.println("Tail");
+        }
+	}
+
+}
